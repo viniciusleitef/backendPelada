6108060025ffd8f7794678d31cbbec526b33eb85
Vinícius Leite <79729340+viniciusleitef@users.noreply.github.com>
2025-12-03 14:11:25 -0300

    Change Uvicorn port from 8000 to 8080

--- main.py
+++ main.py
@@ -30,4 +30,4 @@ app.include_router(auth_router)
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run('main:app', host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run('main:app', host="0.0.0.0", port=8080, reload=True)
